0f40e6f31ab5f1c7dc1e4566ffa68ffaa51035e2
Vladimir Stanley Alvarez <stanleyvladimir2005@hotrmail.com>
2022-09-21 11:00:45 -0600

    GateWay

---
+++ src/main/java/com/mitocode/GatewayConfig.java
@@ -0,0 +1,26 @@
+package com.mitocode;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+	//MODO FUNCIONAL DEL GATEWAY EN CODIGO JAVA
+		/*@Bean
+	    public RouteLocator routeLocator(RouteLocatorBuilder builder, TokenRelayGatewayFilterFactory tokenRelay) {
+	        return builder.routes()
+	                .route("m1-service", r -> r.path("/m1/**")
+	                		.filters(f -> {
+	                			f.rewritePath("/m1/(?<path>.*)", "/${path}");
+	                			return f.filter(tokenRelay.apply());
+	                		})                          
+	                        .uri("http://localhost:8080"))
+	                .route("m2-service", r -> r.path("/m2/**")
+	                		.filters(f -> {
+	                			f.rewritePath("/m2/(?<path>.*)", "/${path}");
+	                			return f.filter(tokenRelay.apply());
+	                		})                          
+	                        .uri("lb://micro2"))
+	                .build();             
+	    }*/
+}
