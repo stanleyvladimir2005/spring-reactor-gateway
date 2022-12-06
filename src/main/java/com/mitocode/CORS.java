b7687cf42963305b44d7d0f71699dc2654dfff66
Vladimir Stanley Alvarez <stanleyvladimir2005@hotrmail.com>
2022-12-06 17:09:31 -0600

    Actualizando proyecto a Spring boot 3

--- src/main/java/com/mitocode/CORS.java
+++ src/main/java/com/mitocode/CORS.java
@@ -1,7 +1,6 @@
 package com.mitocode;
 
 import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
